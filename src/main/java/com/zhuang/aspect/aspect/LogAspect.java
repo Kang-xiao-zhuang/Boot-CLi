3f6fe5fb357cc616754ccc8534abb95a6a5ad819
kangxiaozhuang <2247830091@qq.com>
2023-11-15 16:57:42 +0800

    测试aop切面的before通知

--- src/main/java/com/zhuang/aspect/aspect/LogAspect.java
+++ src/main/java/com/zhuang/aspect/aspect/LogAspect.java
@@ -2,10 +2,11 @@ package com.zhuang.aspect.aspect;
 
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
 
 @Aspect
 @Component
@@ -15,17 +16,20 @@ public class LogAspect {
 
     // 定义切面
     @Pointcut("@annotation(com.zhuang.aspect.annotation.LogAnnotation)")
-    private void logAspect(){
+    private void logAspect() {
 
     }
 
     @Before("logAspect()")
-    public void doAround(JoinPoint jpt) throws Throwable{
+    public void doAround(JoinPoint jpt) throws Throwable {
+        // 获取传过来的参数，在执行service方法前执行
+        Object[] args = jpt.getArgs();
+        System.out.println(Arrays.toString(args));
         log.info(jpt.toString());
     }
 
-   @AfterThrowing(value = "logAspect()",throwing = "e")
-    public void logAfterThrowing(JoinPoint jpt,Exception e){
+    @AfterThrowing(value = "logAspect()", throwing = "e")
+    public void logAfterThrowing(JoinPoint jpt, Exception e) {
         log.error(jpt.getSignature().getName());
         log.info(e.getMessage());
     }
