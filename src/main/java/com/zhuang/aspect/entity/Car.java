cbbffea79f9d69f88a2ad832e331e5ffaac2f42a
kangxiaozhuang <2247830091@qq.com>
2023-11-24 14:37:22 +0800

    jackson操作

--- src/main/java/com/zhuang/aspect/entity/Car.java
+++ src/main/java/com/zhuang/aspect/entity/Car.java
@@ -1,8 +1,10 @@
 package com.zhuang.aspect.entity;
 
 import lombok.Data;
+import lombok.ToString;
 
 @Data
+@ToString
 public class Car {
 
     private String brand = null;
--- src/test/java/com/zhuang/aspect/jackson/JackSonTest.java
+++ src/test/java/com/zhuang/aspect/jackson/JackSonTest.java
@@ -1,11 +1,14 @@
 package com.zhuang.aspect.jackson;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.zhuang.aspect.entity.Car;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 @SpringBootTest
 public class JackSonTest {
@@ -15,16 +18,21 @@ public class JackSonTest {
         ObjectMapper objectMapper = new ObjectMapper();
 
         String carJson = "{ \"brand\" : \"Mercedes\", \"doors\" : 5 }";
+        String jsonArray = "[{\"brand\":\"ford\"}, {\"brand\":\"Fiat\"}]";
+
 
         try {
+            // JSON字符串-->Java对象
             Car car = objectMapper.readValue(carJson, Car.class);
-
-            System.out.println("car brand = " + car.getBrand());
-            System.out.println("car doors = " + car.getDoors());
-        } catch (
-                IOException e) {
+            // JSON数组字符串-->Java对象数组
+            Car[] cars2 = objectMapper.readValue(jsonArray, Car[].class);
+            // JSON数组字符串-->List
+            List<Car> carList = objectMapper.readValue(jsonArray, new TypeReference<List<Car>>(){});
+            System.out.println("car = " + car);
+            System.out.println("cars2 = " + Arrays.toString(cars2));
+            System.out.println("carList = " + carList);
+        } catch (IOException e) {
             e.printStackTrace();
-
         }
 
     }
